b85ebfb7a6a77b565cd1be3ddf316d0e94c45087
Mauro Cendon <mau.cendon@gmail.com>
2021-02-16 10:23:47 +0100

    Update content.js

--- content.js
+++ content.js
@@ -1,9 +1,22 @@
 (function removeBackdrop() {
-  if (document.getElementsByClassName('tp-modal-open').length == 0) {
+  let dialog = document.getElementsByTagName("ec-dialog") || document.getElementsByClassName('tp-modal-open');
+  
+  if (dialog.length == 0) {
     setTimeout(removeBackdrop, 100);
     return;
   }
-  document.getElementsByClassName('tp-modal-open')[0].classList.remove('tp-modal-open');
-  document.getElementsByClassName('tp-modal')[0].remove();
-  document.getElementsByClassName('tp-backdrop')[0].remove();
+  
+  if (document.getElementsByClassName('tp-modal-open').length > 0)
+    document.getElementsByClassName('tp-modal-open')[0].classList.remove('tp-modal-open');
+  
+  if (document.getElementsByClassName('tp-modal').length > 0)
+    document.getElementsByClassName('tp-modal')[0].remove();
+  
+  if (document.getElementsByClassName('tp-backdrop').length > 0)
+    document.getElementsByClassName('tp-backdrop')[0].remove();
+  
+  if (document.getElementsByTagName("ec-dialog").length > 0)
+    document.getElementsByTagName("ec-dialog").item(0).remove();
+  
+  document.getElementsByTagName("body")[0].style.overflowY = "visible";
 })();
